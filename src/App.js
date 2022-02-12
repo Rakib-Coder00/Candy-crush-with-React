46c849ab5830c7f8ed20e6845af36d07974ce9da
Limitless-Rakib <80609225+Rakib-Coder00@users.noreply.github.com>
2022-02-13 01:46:25 +0600

    checking for matches

--- src/App.js
+++ src/App.js
@@ -15,6 +15,52 @@ const App = () => {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([])
 
 
+  const checkForColumnOfFour = () => {
+    for (let i = 0; i < 39; i++) {
+      const ColumnOfFour = [i, i + width,i +width *2, i + width * 3]
+      const decidedColor = currentColorArrangement[i]
+      if (ColumnOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+        ColumnOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
+  const checkForRowOfFour = () => {
+    for (let i = 0; i < 64; i++) {
+      const RowOfFour = [i, i + 1, i+ 2, i + 3]
+      const decidedColor = currentColorArrangement[i]
+      const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 52,  63, 64]
+      if (notValid.includes(i)) continue
+
+      if (RowOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+        RowOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++) {
+      const ColumnOfThree = [i, i + width,i +width *2]
+      const decidedColor = currentColorArrangement[i]
+      if (ColumnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+        ColumnOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
+  const checkForRowOfThree = () => {
+    for (let i = 0; i < 64; i++) {
+      const RowOfThree = [i, i + 1, i + 2]
+      const decidedColor = currentColorArrangement[i]
+      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+      if (notValid.includes(i)) continue
+
+      if (RowOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+        RowOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
   const createBoard = ()=>{
     const randomColorArrangement = []
 
@@ -33,6 +79,19 @@ const App = () => {
     createBoard()
   }, [])
 
+  useEffect(() => {
+    const timer = setInterval(()=>{
+      checkForColumnOfFour()
+      checkForRowOfFour()
+      checkForColumnOfThree()
+      checkForRowOfThree()
+
+      setCurrentColorArrangement([ ...currentColorArrangement])
+    },100)
+    return () => clearInterval(timer)
+  }, [checkForColumnOfFour, checkForRowOfFour ,checkForColumnOfThree, checkForRowOfThree, currentColorArrangement])
+  
+
   console.log(currentColorArrangement);
 
   return (
@@ -41,7 +100,8 @@ const App = () => {
         {currentColorArrangement.map((candyColor, index) =>(
           <img
             key={index}
-            style={{ backgroundColor: candyColor}}
+            style={{backgroundColor: candyColor}}
+            alt={candyColor}
           />
         ))}
      </div>
@@ -50,3 +110,4 @@ const App = () => {
 }
 
 export default App;
+ 
