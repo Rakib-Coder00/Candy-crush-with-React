b4fea9456ddc9cb950f5af2c205d86026ee9f2c8
Limitless-Rakib <80609225+Rakib-Coder00@users.noreply.github.com>
2022-02-13 17:25:48 +0600

    moving candies down

--- src/App.js
+++ src/App.js
@@ -61,6 +61,24 @@ const App = () => {
     }
   }
 
+
+
+  const moveIntoSquareBelow = ()=>{
+    for (let i = 0; i < 64 - width; i++){
+      const firstRow = [0, 2, 3, 4, 5, 6, 7]
+      const isFirstRow = firstRow.includes(i)
+      if (isFirstRow && currentColorArrangement[i] === '') {
+        let randomNumber = Math.floor(Math.random() * candyColors.length)
+        currentColorArrangement[i] = candyColors[randomNumber]
+      }
+
+      if ((currentColorArrangement[i + width]) === '') {
+        currentColorArrangement[i + width] = currentColorArrangement[i]
+        currentColorArrangement[i] = ''
+      }
+    }
+  }
+
   const createBoard = ()=>{
     const randomColorArrangement = []
 
@@ -85,6 +103,7 @@ const App = () => {
       checkForRowOfFour()
       checkForColumnOfThree()
       checkForRowOfThree()
+      moveIntoSquareBelow()
 
       setCurrentColorArrangement([ ...currentColorArrangement])
     },100)
